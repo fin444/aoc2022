7b275e16a3fee3319026e29a3c62beb5ad8ff4bf
fin444 <fin444@user.noreply.github.com>
2022-12-02 00:24:57 -0500

    day 2

---
+++ 2-2.py
@@ -0,0 +1,40 @@
+lines = [line.replace("\n", "") for line in open("aoc_input.txt", "r").readlines()]
+
+score = 0
+
+for line in lines:
+	roundscore = 0
+	attack = line.split(" ")[0]
+	defend = line.split(" ")[1]
+	if attack == "A":
+		if defend == "X":
+			roundscore += 3
+		elif defend == "Y":
+			roundscore += 1
+		elif defend == "Z":
+			roundscore += 2
+	elif attack == "B":
+		if defend == "X":
+			roundscore += 1
+		elif defend == "Y":
+			roundscore += 2
+		elif defend == "Z":
+			roundscore += 3
+	elif attack == "C":
+		if defend == "X":
+			roundscore += 2
+		elif defend == "Y":
+			roundscore += 3
+		elif defend == "Z":
+			roundscore += 1
+	
+	if defend == "X":
+		roundscore += 0
+	elif defend == "Y":
+		roundscore += 3
+	elif defend == "Z":
+		roundscore += 6
+	
+	score += roundscore
+
+print(score)
